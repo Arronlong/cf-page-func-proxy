a470be0a14f6fafad9323faae43a299bf78135f3
Arronlong <cclsuperstar@126.com>
2022-06-25 23:26:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ export default {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
         //url.hostname="google.com";
-        url.hostname="server.arronlong.com";
+        url.hostname="servers.arronlong.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
