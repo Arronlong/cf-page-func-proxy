1863b47b08c256571564a9bfb2e52985c8825130
Arronlong <cclsuperstar@126.com>
2023-07-19 10:46:42 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ export default {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
         //url.hostname="google.com";
-        url.hostname="bing.com" ;
+        url.hostname="ddg-api.herokuapp.com" ;
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
